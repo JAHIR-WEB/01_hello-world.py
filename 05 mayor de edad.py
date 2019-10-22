175aa8e38a5cadfd3c3eac0cacfee64a80f31c65
JAHIR-WEB <56598580+JAHIR-WEB@users.noreply.github.com>
2019-10-22 11:10:10 -0500

    Add files via upload

---
+++ 05 mayor de edad.py
@@ -0,0 +1,11 @@
+Nombre = input ("cual es tu nombre?")
+Apellido = input ("Cual es tu apellido?")
+Domicilio = input ("donde vives?")
+Edad = input ("cual es tu Edad?")
+print("hola",Nombre, Apellido,"Tu edad es",Edad,"tu informacion dice que vives en",Domicilio)
+
+if Edad >= "18":
+    print ("es mayor de edad")
+else:
+    print ("es menor de edad")
+    
