5c342c5a2119bbecb235d3f0c027349e38546a56
JAHIR-WEB <56598580+JAHIR-WEB@users.noreply.github.com>
2019-10-22 09:19:54 -0500

    Add files via upload

---
+++ DEBER1.py
@@ -0,0 +1,21 @@
+Python 3.8.0 (tags/v3.8.0:fa919fd, Oct 14 2019, 19:21:23) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> BRICS=["Brazil","Russia","India","China","South Africa"]
+>>> type(BRICS)
+<class 'list'>
+>>> len(BRICS)
+5
+>>> BRICS
+['Brazil', 'Russia', 'India', 'China', 'South Africa']
+>>> capitals={'Brazil':'Brasilia','Russia':'Moscow','India':'New Delhi','China':'Beijin','South Africa':'Pretoria,Cape Town,Bloemfontein',}
+>>> capitals['Brazil']
+'Brasilia'
+>>> capitals['Russia']
+'Moscow'
+>>> capitals['India']
+'New Delhi'
+>>> capitals['China']
+'Beijin'
+>>> capitals['South Africa']
+'Pretoria,Cape Town,Bloemfontein'
+>>> 
